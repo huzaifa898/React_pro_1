b2e9d83626182cfd02b841de55bb14fc2a1a33bc
huzaifa898 <122553707+huzaifa898@users.noreply.github.com>
2023-03-11 23:48:09 +0500

    just make a basic card through jsx

--- project/src/index.js
+++ project/src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="cards">
+      <div className="card">
+        <img src="https://i.pinimg.com/originals/cd/67/78/cd67786db8f2bef016b9d4e924ecaf03.jpg" alt="myPic"></img>
+        <div className="card__info">
+          <span className="card_category"> A netflix Original Series</span>
+          <h3 className="card_tittle">DARK</h3>
+          <a href="https://www.netflix.com/pk/title/80100172" target="_blank">
+            <button>Watch Now</button>
+          </a>
+        </div>
+      </div>
+    </div>
   </React.StrictMode>
 );
 
