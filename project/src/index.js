d3f2de04a4dcd944be0d9d56cfd32c9b7bf630a1
huzaifa898 <122553707+huzaifa898@users.noreply.github.com>
2023-03-12 17:45:03 +0500

    completed

--- project/src/index.js
+++ project/src/index.js
@@ -4,7 +4,17 @@ import './index.css';
 import Cards from "./Cards";
 ReactDOM.render(
   <>
-  <h1 className="heading_style">LIST OF TOP 50 NETFLIX SERIES IN 2022</h1>
+  <nav>
+        <div class="logo">Dicet Tv</div>
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">BLOGS</a></li>
+            <li><a href="#">TRAILERS</a></li>
+            <li><a href="#">CONTACT US</a></li>
+        </ul>
+      </nav>
+  <h1 className="heading_style">WATCH FREE NETFLIX AND AMAZON PRIME SERIES`ES</h1>
     <Cards
       imgSrc="https://i.pinimg.com/originals/cd/67/78/cd67786db8f2bef016b9d4e924ecaf03.jpg"
       title="A netflix Original Series"
@@ -22,6 +32,78 @@ ReactDOM.render(
     title="A netflix Originals series"
     sName="ETRACURRICULAR"
     link="https://www.netflix.com/pk/title/80990668"
+    />
+    <Cards
+    imgSrc="https://playdesi.net/wp-content/uploads/2020/04/the-family-man-review-1140x445.jpeg"
+    title="A netflix Originals series"
+    sName="THE FAMILY MAN"
+    link="https://playdesi.net/the-family-man-episode-1-watch-online/"
+    />
+     <Cards
+    imgSrc="https://playdesi.net/wp-content/uploads/2020/04/sacred-1140x445.jpg"
+    title="A netflix Originals series"
+    sName="SCARED GAMES"
+    link="https://playdesi.net/watch-online/netflix/sacred-games-season-1-netflix//"
+    />
+    <Cards
+    imgSrc="https://playdesi.net/wp-content/uploads/2020/04/a6s565565-1140x445.jpg"
+    title="A netflix Originals series"
+    sName="SHE"
+    link="https://playdesi.net/watch-online/netflix/she-neflix/"
+    />
+    <Cards
+    imgSrc="https://playdesi.net/wp-content/uploads/2023/02/Class-Season-1-Poster-1140x445.jpg"
+    title="A netflix Originals series"
+    sName="CLASS"
+    link="https://playdesi.net/watch-online/netflix/class-season-1/"
+    />
+     <Cards
+    imgSrc="https://playdesi.net/wp-content/uploads/2021/02/Money-Heist-1-1140x445.jpg"
+    title="A netflix Originals series"
+    sName="MONEY HEIST"
+    link="https://playdesi.net/?s=MONEY+HEIST"
+    />
+    <Cards
+    imgSrc="https://playdesi.net/wp-content/uploads/2023/02/Farzi-S01-Poster-1140x445.webp"
+    title="A Amazon Originals series"
+    sName="FARZI"
+    link="https://playdesi.net/watch-online/amazon-prime/farzi-season-1/"
+    />
+    <Cards
+    imgSrc="https://playdesi.net/wp-content/uploads/2020/04/HOSTEL_DAZE_1575880320542-1140x445.jpg"
+    title="A Amazon Originals series"
+    sName="HOSTEL DAZE"
+    link="https://playdesi.net/watch-online/amazon-prime/hostel-daze-amazon/"
+    />
+    <Cards
+    imgSrc="https://playdesi.net/wp-content/uploads/2020/04/mirzapur-prime-1200-1140x445.jpg"
+    title="A Amazon Originals series"
+    sName="MIRZIPUR"
+    link="https://playdesi.net/watch-online/amazon-prime/mirzapur-season-1-amazon/"
+    />
+    <Cards
+    imgSrc="https://playdesi.net/wp-content/uploads/2020/04/breathe-1140x445.jpg"
+    title="A Amazon Originals series"
+    sName="BREATHE"
+    link="https://playdesi.net/watch-online/amazon-prime/breathe-amazon/"
+    />
+    <Cards
+    imgSrc="https://playdesi.net/wp-content/uploads/2020/05/Betaal-1140x445.jpg"
+    title="A Netflix Originals series"
+    sName="BETAAL"
+    link="https://playdesi.net/watch-online/netflix/betaal-netflix/"
+    />
+    <Cards
+    imgSrc="https://playdesi.net/wp-content/uploads/2021/06/ray-1140x445.png"
+    title="A Netflix Originals series"
+    sName="RAY"
+    link="https://playdesi.net/watch-online/netflix/ray//"
+    />
+    <Cards
+    imgSrc="https://playdesi.net/wp-content/uploads/2023/01/Trial-By-Fire-2023-Poster-1140x445.jpg"
+    title="A Netflix Originals series"
+    sName="TRIAL-BY-FIRE"
+    link="https://playdesi.net/watch-online/netflix/trial-by-fire-season-1/"
     />
     
    
